bdf37e6633460e70f1726cb696653a4d17cbbf4e
Mohamed-Khaterr <mohamed.15751@gmail.com>
2023-01-02 15:14:48 +0200

    initial commit

--- Basic Banking App/Controller/HomeViewController.swift
+++ Basic Banking App/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Basic Banking App
 //
 //  Created by Khater on 1/2/23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
---
+++ Basic Banking App/Controller/TransfareViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TransfareViewController.swift
+//  Basic Banking App
+//
+//  Created by Khater on 1/2/23.
+//
+
+import UIKit
+import SQLite3
+
+class TransfareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
