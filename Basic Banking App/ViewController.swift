a95c6feb55cbfb12cbe2ccfe7b707c6bf8a7990e
Mohamed-Khaterr <mohamed.15751@gmail.com>
2023-01-02 13:35:52 +0200

    Initial Commit

---
+++ Basic Banking App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Banking App
+//
+//  Created by Khater on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
